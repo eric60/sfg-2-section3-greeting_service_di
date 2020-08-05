42df56b3673069f4001145ac103cb56ebc537e51
Eric Shi <ericshi98@yahoo.com>
2020-08-05 12:47:02 -0400

    added Qualifiers and profiles.

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,13 +1,35 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+		MyController myController = (MyController) context.getBean("myController");
+		System.out.println("---------- Primary Bean");
+		String greeting = myController.getGreeting();
+		System.out.println(greeting);
+
+		System.out.println("---------- Profile Beans");
+		I18nController i18nController = (I18nController) context.getBean("i18nController");
+		System.out.println(i18nController.getGreeting());
+
+		System.out.println("---------- Property Injected Controller");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) context.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("---------- Setter Injected Controller");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) context.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("---------- Controller Injected Controller");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) context.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
 	}
 
 }
---
+++ src/test/java/guru/springframework/sfgdi/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,20 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SetterInjectedControllerTest {
+    SetterInjectedController setterInjectedController;
+
+    @BeforeEach
+    void setUp() {
+        setterInjectedController = new SetterInjectedController();
+        setterInjectedController.setGreetingService(new ConstructorGreetingService());
+    }
+
+    @Test
+    void Test_getGreeting() {
+        System.out.println(setterInjectedController.getGreeting());
+    }
+}
